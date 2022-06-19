c97c44ef34b12bdb389b70ee2abb588be208b188
polluxtroy3758 <2147396+polluxtroy3758@users.noreply.github.com>
2022-06-19 15:23:22 +0200

    end chapter

---
+++ repository.py
@@ -0,0 +1,44 @@
+import abc
+from typing import List
+
+from sqlalchemy.orm import Session
+
+import model
+
+
+class AbstractRepository(abc.ABC):
+    @abc.abstractmethod
+    def add(self, batch: model.Batch):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, reference: str) -> model.Batch:
+        raise NotImplementedError
+
+
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session: Session):
+        self.session = session
+
+    def add(self, batch: model.Batch):
+        self.session.add(batch)
+
+    def get(self, reference: str) -> model.Batch:
+        return self.session.query(model.Batch).filter_by(reference=reference).one()
+
+    def list(self) -> List[model.Batch]:
+        return self.session.query(model.Batch).all()
+
+
+class FakeRepository(AbstractRepository):
+    def __init__(self, batches: List[model.Batch]):
+        self._batches = set(batches)
+
+    def add(self, batch: model.Batch):
+        self._batches.add(batch)
+
+    def get(self, reference: str) -> model.Batch:
+        return next(b for b in self._batches if b.reference == reference)
+
+    def list(self) -> List[model.Batch]:
+        return list(self._batches)
